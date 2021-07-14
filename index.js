4019764c54850d2f7e1aa518c95436f8629f0e33
Vladimir Blinov <zemetrans21@yandex.ru>
2021-07-15 01:41:58 +0300

    feat: создать схемы и модели для базы данных

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 //  задаём порт (ведь мы его вроде как не передаем в окружение)
 const { PORT = 3000 } = process.env;
+
+// подключаемся к серверу mongo
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// подключаем мидлвары, роуты и всё остальное...
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
