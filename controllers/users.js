48e1af2aa894bf0342d8ed3aba0ee3f03423b28d
Vladimir Blinov <zemetrans21@yandex.ru>
2021-07-23 16:23:02 +0300

    feat: сделать первую версию контроллера

---
+++ controllers/users.js
@@ -0,0 +1,35 @@
+const User = require('../models/user');
+
+//колбек для получения всех пользователей
+const getAllUsers = (req, res) => {
+  User.find({})
+    .then(users => {
+      res.status(200).send({data: users});
+    })
+    .catch(err => {
+      res.status(500).send({message: 'Что-то пошло не так'});
+    });
+}
+
+//колбек для получения определённого пользователя
+const getUser = (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      res.status(200).send({data: user});
+    })
+    .catch(err => {
+      res.status(500).send({message: 'Что-то пошло не так'});
+    });
+}
+
+//колбек для создания нового пользователя
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({name, about, avatar})
+    .then(user => res.send({data: user}))
+    .catch(err => {
+      res.status(500).send({message: 'Что-то пошло не так'});
+    });
+}
+
+module.exports = { getAllUsers, getUser, createUser };
