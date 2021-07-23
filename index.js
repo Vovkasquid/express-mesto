f41a07b15dea27eb446632ecb98a21bb8e64bcea
Vladimir Blinov <zemetrans21@yandex.ru>
2021-07-23 21:19:05 +0300

    feat: добавить костыль авторизации в виде мидлвары

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const usersRoute = require('./routes/users')
+const usersRoute = require("./routes/users");
 
 //  задаём порт (ведь мы его вроде как не передаем в окружение)
 const { PORT = 3000 } = process.env;
 
 // UserID
-//60faca1425a4332fafbed7f5
+// 60faca1425a4332fafbed7f5
 
 // подключаемся к серверу mongo
 mongoose.connect("mongodb://localhost:27017/mestodb", {
@@ -20,11 +20,19 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 });
 
 // подключаем мидлвары, роуты и всё остальное...
-//bodyparser теперь часть экспресса, поэтому подключаем его так
+// bodyparser теперь часть экспресса, поэтому подключаем его так
 app.use(express.json());
 
-//Прописываем маршруты
-app.use('/', usersRoute);
+// костыль для авторизации. В ПР14 будет удалён
+app.use((req, res, next) => {
+  req.user = {
+    _id: "60faca1425a4332fafbed7f5",
+  };
+
+  next();
+});
+// Прописываем маршруты
+app.use("/", usersRoute);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
-const router = require('express').Router();
-const { getAllUsers, getUser, createUser } = require('../controllers/users');
+const router = require("express").Router();
+const { getAllUsers, getUser, createUser } = require("../controllers/users");
 /*
 GET /users — возвращает всех пользователей
 GET /users/:userId - возвращает пользователя по _id
 POST /users — создаёт пользователя */
 
-router.get('/users', getAllUsers);
+router.get("/users", getAllUsers);
 
-router.get('/users/:userId', getUser);
+router.get("/users/:userId", getUser);
 
-router.post('/users', createUser);
+router.post("/users", createUser);
 
 module.exports = router;
