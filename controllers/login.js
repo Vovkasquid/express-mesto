85c1f1b55898fcd9b49cc76d4dfb5cbf3617a3d8
Vladimir Blinov <zemetrans21@yandex.ru>
2021-08-07 19:32:31 +0300

    feat: сделать обработку ошибок в login

--- controllers/login.js
+++ controllers/login.js
@@ -2,17 +2,18 @@ const bcrypt = require("bcryptjs"); // импортируем bcrypt
 const jwt = require("jsonwebtoken"); // импортируем модуль jsonwebtoken
 const User = require("../models/user");
 
-const ERROR_CODE_UNAUTHORIZED = 401;
-const ERROR_CODE_DEFAULT_ERROR = 500;
+const Error401 = require("../errors/Error401");
+const Error500 = require("../errors/Error500");
 
-const checkLogin = (req, res) => {
+const ERROR_CODE_UNAUTHORIZED = 401;
+
+const checkLogin = (req, res, next) => {
   const { email, password } = req.body;
   User.findOne({ email }).select("+password")
     .orFail(() => {
       // Если мы здесь, значит запрос в базе ничего не нашёл
       // Бросаем ошибку и попадаем в catch
-      const error = new Error(`Пользователь с email: ${email} не существует`);
-      error.statusCode = ERROR_CODE_UNAUTHORIZED;
+      const error = new Error401(`Пользователь с email: ${email} не существует`);
       throw error;
     })
     .then((user) => {
@@ -21,21 +22,26 @@ const checkLogin = (req, res) => {
         .then((matched) => {
           if (!matched) {
             // Если мы здесь, значит Хэши не совпали. Бросаем ошибку и уходим в catch
-            const error = new Error("Введён неправильный пароль");
-            error.statusCode = ERROR_CODE_UNAUTHORIZED;
+            const error = new Error401("Введён неправильный пароль");
             throw error;
           }
           // Необходимо создать токен и отправить его пользователю
           const token = jwt.sign({ _id: user._id }, "strongest-key-ever", { expiresIn: "7d" });
           res.send({ token });
         })
-        .catch(() => res.status(ERROR_CODE_DEFAULT_ERROR).send({ message: "Что-то пошло не так :(" }));
+        .catch((err) => {
+          if (err.statusCode === ERROR_CODE_UNAUTHORIZED) {
+            next(err);
+          } else {
+            next(new Error500("Что-то пошло не так :("));
+          }
+        });
     })
     .catch((err) => {
       if (err.statusCode === ERROR_CODE_UNAUTHORIZED) {
-        res.status(ERROR_CODE_UNAUTHORIZED).send({ message: err.message });
+        next(err);
       } else {
-        res.status(ERROR_CODE_DEFAULT_ERROR).send({ message: "Что-то пошло не так :(" });
+        next(new Error500("Что-то пошло не так :("));
       }
     });
 };
