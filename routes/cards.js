b8e9df1d1a5b56a644e8fc3603b66ba2028d9080
Vladimir Blinov <zemetrans21@yandex.ru>
2021-08-08 10:13:57 +0300

    feat: защитить валидацией cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
 const {
   getAllCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require("../controllers/cards");
@@ -12,12 +13,29 @@ const {
 
 router.get("/cards", getAllCards);
 
-router.post("/cards",createCard);
+router.post("/cards", celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().required(),
+  }),
+}), createCard);
 
-router.delete("/cards/:cardId", deleteCard);
+router.delete("/cards/:cardId", celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), deleteCard);
 
-router.put("/cards/:cardId/likes", likeCard);
+router.put("/cards/:cardId/likes", celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), likeCard);
 
-router.delete("/cards/:cardId/likes", dislikeCard);
+router.delete("/cards/:cardId/likes", celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), dislikeCard);
 
 module.exports = router;
