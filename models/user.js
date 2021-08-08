40888788e9e315b41f43777c5a46188890aca6da
Vladimir Blinov <zemetrans21@yandex.ru>
2021-08-08 11:07:17 +0300

    feat: добавить валидацию аватары на уровне схемы

--- models/user.js
+++ models/user.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
-   // match: /^https?:\/\/(www.)?[a-zA-Z0-9-.]+\.[a-zA-Z]{2,}([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+)*#*$/,
+    match: /^https?:\/\/(www.)?[a-zA-Z0-9-.]+\.[a-zA-Z]{2,}([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+)*#*$/,
   },
   email: {
     type: String,
