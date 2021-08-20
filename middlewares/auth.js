1aa6d273f0f3c69e51817b52f5306eaecdaeb0cb
Vladimir Blinov <zemetrans21@yandex.ru>
2021-08-20 20:45:40 +0300

    fix: исправить код ошибки при авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken"); // импортируем модуль jsonwebtoken
-const Error403 = require("../errors/Error403");
+const Error401 = require("../errors/Error401");
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   // Проверяем есть ли заголовок и начинается ли он с Bearer
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return next(new Error403("Необходима авторизация"));
+    return next(new Error401("Необходима авторизация"));
   }
   // Отделяем токен от Bearer
   const token = authorization.replace("Bearer ", "");
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
     // Вытаскиваем айди из токена
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new Error403("Необходима авторизация"));
+    next(new Error401("Необходима авторизация"));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
